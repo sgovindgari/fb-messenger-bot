77818a9052ab587f1827e8f49d62f9c4faaee76a
Spandana Govindgari <spandana@Spandanas-MacBook-Pro.local>
2019-10-02 14:37:09 -0700

    updated version for graph api

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    r = requests.post("https://graph.facebook.com/v4.0/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
