ede08ac483c94aa7da507582303e9f8031619696
satyajeetgawas <satyajeet.gawas@gmail.com>
2019-10-04 10:10:25 -0700

    changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 import json
@@ -7,6 +8,7 @@ from datetime import datetime
 import requests
 from flask import jsonify
 from flask import Flask, request
+import threading
 
 if sys.version_info[0] >= 3:
     unicode = str
@@ -18,6 +20,8 @@ plaid_server = "https://peaceful-fortress-19275.herokuapp.com/liabilities"
 # hardcoding this token for now
 # TODO - use cookies to pass this back to messenger
 access_token = "access-sandbox-28e8315a-845e-42bb-8848-6683411cfc9f"
+
+
 
 
 @app.route('/', methods=['GET'])
@@ -45,19 +49,39 @@ def webhook():
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
 
+                log(messaging_event)
+
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    # if 'options' in message_text:
-                    #     log('called options')
-                    #     send_all_options(sender_id)
-                    # else:
-                    log(message_text)
-                    send_message(sender_id, "Welcome to Pixy. We help you lower and pay off your student loan and credit card debt faster.")
-                    add_bank_account(sender_id)
+                    if 'options' in message_text:
+                        log('called options')
+                        send_all_options(sender_id)
+                    elif 'add account' in message_text:
+                        add_bank_account(sender_id)
+                    elif 'refinance' in message_text:
+                        refinance_loan(sender_id)
+                    elif 'insights' in message_text:
+                        insights(sender_id)
+                    elif 'spend' in message_text:
+                        habit_forming(sender_id)
+                    elif 'new debt' in message_text:
+                        updated_debt(sender_id)
+                    elif 'yes' in message_text:
+                        send_message_with_picture('https://media1.tenor.com/images/04052e9d28831ce512093906edec28f6/tenor.gif')
+                        send_message('Pixy will process your payment accordingly.')
+                    elif 'demo' in message_text:
+                        thread1 = threading.Thread(target=workflow, args=(sender_id,))
+                        thread1.start()
+                        log('called workflow')
+                    else:
+                        send_message(sender_id, "Welcome to Pixy. 😀")
+                        send_message(sender_id, "I will find the best ways to help you manage you student debt 💰. I can recommend the optimal payment amounts, remind you of upcoming payments, help you manage your expenses and increase savings")
+                        add_bank_account(sender_id)
+                        
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -66,6 +90,7 @@ def webhook():
                     pass
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    log('received postback')
                     if messaging_event['payload'] is not None:
                         
                         event = messaging_event['postback']['payload']
@@ -76,6 +101,7 @@ def webhook():
                         elif event == 'habits':
                             habit_forming(sender_id)
                         elif event == 'refinance':
+                            log('refinance')
                             refinance_loan(sender_id)
                         else:
                             pass
@@ -103,21 +129,8 @@ def send_all_options(recipient_id):
                         "template_type":"generic",
                         "elements":[
                             {
-                                "title":"Welcome!",
-                                "image_url":"https://s3.amazonaws.com/gethyped.io/pixycover.png",
-                                "subtitle":"We help you manage your student loans and credit card debt.",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peaceful-fortress-19275.herokuapp.com/",
-                                    "webview_height_ratio": "tall",
-                                },
+                                "title":"Here are some ways we can help you! Select an option.",
                             "buttons":[
-                                {
-                                    "type":"web_url",
-                                    "url":"https://peaceful-fortress-19275.herokuapp.com/",
-                                    "title":"Add account",
-                                    "webview_height_ratio": "tall",
-                                },
                                 {
                                     "type":"postback",
                                     "title":"Analyze expenses",
@@ -140,11 +153,11 @@ def send_all_options(recipient_id):
         }
     })
 
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    r = requests.post("https://graph.facebook.com/v4.0/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
-        log(r.text)
-
+        print(r.text)
+        #log(r.text)
 
 def add_bank_account(recipient_id):
     params = {
@@ -165,7 +178,7 @@ def add_bank_account(recipient_id):
                     "payload":{
                         "template_type":"generic",
                         "elements":[{
-                            "title":"Hi!",
+                            "title":"Link your bank accounts to get started",
                             "image_url":"https://s3.amazonaws.com/gethyped.io/pixycover.png",
                             "subtitle":"We help you manage your student loans and credit card debt.",
                             "default_action": {
@@ -176,19 +189,7 @@ def add_bank_account(recipient_id):
                         "buttons":[{
                             "type":"web_url",
                             "url":"https://peaceful-fortress-19275.herokuapp.com/",
-                            "title":"Add account",
-                            "webview_height_ratio": "tall",
-                        },
-                        {
-                            "type":"web_url",
-                            "url":"https://www.nerdwallet.com/blog/pay-off-debt/",
-                            "title":"Credit Card Guide",
-                            "webview_height_ratio": "tall",
-                        },
-                        {
-                            "type":"web_url",
-                            "url":"https://www.creditkarma.com/advice/i/student-loans-101/",
-                            "title":"Student Loan Guide",
+                            "title":"Link bank accounts",
                             "webview_height_ratio": "tall",
                         }]
                     }]
@@ -204,6 +205,19 @@ def add_bank_account(recipient_id):
 
 # TODO - show liabilities
 # TODO - show workflows 
+
+# {
+#     "type":"web_url",
+#     "url":"https://www.nerdwallet.com/blog/pay-off-debt/",
+#     "title":"Credit Card Guide",
+#     "webview_height_ratio": "tall",
+# },
+# {
+#     "type":"web_url",
+#     "url":"https://www.creditkarma.com/advice/i/student-loans-101/",
+#     "title":"Student Loan Guide",
+#     "webview_height_ratio": "tall",
+# }
 
 def show_current_liabilities(recipient_id):
     response = json.loads(requests.get(plaid_server))
@@ -283,7 +297,7 @@ def refinance_loan(recipient_id):
                         {
                             "type":"web_url",
                             "url":"https://www.commonbond.co/affiliate/student-loan-hero",
-                            "title":"Common Bond 2.14-8.24%",
+                            "title":"CommonBond 2.14-8.24%",
                             "webview_height_ratio": "tall",
                         }]
                 }
@@ -296,9 +310,63 @@ def refinance_loan(recipient_id):
         log(r.status_code)
         log(r.text)
 
+def workflow(recipient_id):
+
+    send_message(recipient_id, "Your account has been successfully connected! 🎉")
+
+    send_message(recipient_id, "You can add more accounts anytime by sending \"add account\"")
+
+    send_message_with_picture(recipient_id, "https://media.giphy.com/media/zNSX3G2LztEyY/giphy.gif")
+
+    send_message(recipient_id, "Building your financial picture...")
+
+    time.sleep(20)
+
+    insights(recipient_id)
+
+    time.sleep(20)
+
+    habit_forming(recipient_id)
+
+    time.sleep(20)
+
+    updated_debt(recipient_id)
+
+    time.sleep(25)
+
+    refinance_loan(recipient_id)
+
 
 def habit_forming(recipient_id):
-    pass
+    send_message(
+        recipient_id,
+        "Hey there, hope your day is going great! It looks like you spent $98 on amazon.com this month.  You can save $5 in total interest if you pay $98 towards your loan. Would you like me to save some money towards your student loan?"
+    )
+
+def updated_debt(recipient_id):
+    send_message(recipient_id, 
+        "You paid $300.35 this month towards your student loan. Oustanding loan amount is now $24,328! Keep up the good work!! 🎊"
+    )
+
+    debt_breakdown(recipient_id, 300.35)
+
+def debt_breakdown(recipient_id, minus):
+    send_message(recipient_id, 
+        "Average monthly income: $3320\n" +
+        "Average monthly expenses: $2020\n" +
+        "Breakdown of your debt: {:.2f}\n\n".format((27503.9-minus)) + 
+        "Credit cards: 2775.55\n" + 
+        "   APR: 15.24\n" + 
+        "   Amount: 1562.32\n" + 
+        "   APR: 27.95\n"+
+        "   Amount: 56.22\n"+
+        "   APR: 12.5\n"+
+        "   Amount: 157.01\n"+
+        "   APR: 0\n"+
+        "   Amount: 1000\n\n" + 
+        "Student Loans: {:.2f}\n".format((24728.35-minus)) + 
+        "Interest rate: 5.25%\n"
+    )
 
 def insights(recipient_id):
     # Monthly payments
@@ -313,20 +381,42 @@ def insights(recipient_id):
     # Balance so far
     # Suggested amount to pay
 
-    send_message(recipient_id, 
-        "Breakdown of your debt: 27503.9\n\n" + 
-        "Credit cards: 2775.55\n" + 
-        "\tAPR: 15.24" + 
-        "\tAmount: 1562.32\n" + 
-        "\tAPR: 27.95\n"+
-        "\tAmount: 56.22\n"+
-        "\tAPR: 12.5\n"+
-        "\tAmount: 157.01\n"+
-        "\tAPR: 0\n"+
-        "\tAmount: 1000\n\n" + 
-        "Student Loans: 24728.35\n" + 
-        "Interest rate: 5.25%\n"
+    debt_breakdown(recipient_id, 0)
+
+    send_message_with_picture(recipient_id, "https://s3.amazonaws.com/gethyped.io/pixydebt2.jpg")
+
+    send_message(
+        recipient_id,
+        "We found that you can save $983 in interest by making $102 additional monthly payment towards your Chase student loan. Do you want me to increase your monthly payment?"
     )
+
+def send_message_with_picture(recipient_id, url):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message":{
+            "attachment": {
+                "type": "image",
+                "payload": {
+                    "url": url,
+                }
+            }    
+        }
+    })
+
+    r = requests.post("https://graph.facebook.com/v4.0/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
 def send_message_with_button(recipient_id, message_text, button_url, button_text):
@@ -415,3 +505,4 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
+
